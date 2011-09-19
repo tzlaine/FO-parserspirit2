d8ff62dc23ddefe08bb67bd037ebbc06a64ee26f
Zach Laine <whatwasthataddress@gmail.com>
2011-09-18 21:23:02 -0500

    Added forgotten value_ref_test_rules.cpp.

---
+++ value_ref_test_rules.cpp
@@ -0,0 +1,12 @@
+#include "test.h"
+
+
+value_ref_test_rules::value_ref_test_rules(const parse::lexer& tok)
+{
+    namespace qi = boost::spirit::qi;
+    namespace phoenix = boost::phoenix;
+    using qi::_1;
+    using phoenix::val;
+
+    // TODO
+}
