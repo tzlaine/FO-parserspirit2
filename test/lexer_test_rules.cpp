7290e96f96e83da5b99163d368384663be8bbdca
Zach Laine <whatwasthataddress@gmail.com>
2011-10-28 19:24:54 -0500

    Changed the lexer test output slightly to make it usable as a standalone tokenizer.

--- test/lexer_test_rules.cpp
+++ test/lexer_test_rules.cpp
@@ -6,6 +6,8 @@ lexer_test_rules::lexer_test_rules()
     namespace qi = boost::spirit::qi;
     namespace phoenix = boost::phoenix;
     qi::_1_type _1;
+    qi::char_type char_;
+    qi::as_string_type as_string;
     using phoenix::val;
 
     const parse::lexer& tok = parse::lexer::instance();
@@ -15,40 +17,40 @@ lexer_test_rules::lexer_test_rules()
                    tok.inline_comment [ std::cout << val("<INLINE COMMENT>\n") ]
                 |  tok.end_of_line_comment [ std::cout << val("<END OF LINE COMMENT>\n") ]
 
-                |  tok.bool_ [ std::cout << _1 << " BOOL\n" ]
-                |  tok.int_ [ std::cout << _1 << " INT\n" ]
-                |  tok.double_ [ std::cout << _1 << " DOUBLE\n" ]
-                |  tok.string [ std::cout << _1 << " STRING\n" ]
+                |  as_string[ tok.bool_ ] [ std::cout << _1 << "\n" ]
+                |  as_string[ tok.int_ ] [ std::cout << _1 << "\n" ]
+                |  as_string[ tok.double_ ] [ std::cout << _1 << "\n" ]
+                |  tok.string [ std::cout << _1 << "\n" ]
 
-                |  tok.planet_size_enum [ std::cout << _1 << " PLANET_SIZE\n" ]
-                |  tok.planet_type_enum [ std::cout << _1 << " PLANET_TYPE\n" ]
-                |  tok.planet_environment_enum [ std::cout << _1 << " PLANET_ENVIRONMENT\n" ]
-                |  tok.universe_object_type_enum [ std::cout << _1 << " UNIVERSE_OBJECT_TYPE\n" ]
-                |  tok.star_type_enum [ std::cout << _1 << " STAR_TYPE\n" ]
-                |  tok.empire_affiliation_type_enum [ std::cout << _1 << " EMPIRE_AFFILIATION_TYPE\n" ]
-                |  tok.unlockable_item_type_enum [ std::cout << _1 << " UNLOCKABLE_ITEM_TYPE\n" ]
-                |  tok.tech_type_enum [ std::cout << _1 << " TECH_TYPE\n" ]
-                |  tok.combat_fighter_type_enum [ std::cout << _1 << " COMBAT_FIGHTER_TYPE\n" ]
-                |  tok.ship_part_class_enum [ std::cout << _1 << " SHIP_PART_CLASS\n" ]
-                |  tok.ship_slot_type_enum [ std::cout << _1 << " SHIP_SLOT_TYPE\n" ]
-                |  tok.capture_result_enum [ std::cout << _1 << " CAPTURE_RESULT\n" ]
-                |  tok.statistic_type_enum [ std::cout << _1 << " STATISTIC_TYPE\n" ]
+                |  as_string[ tok.planet_size_enum ] [ std::cout << _1 << "\n" ]
+                |  as_string[ tok.planet_type_enum ] [ std::cout << _1 << "\n" ]
+                |  as_string[ tok.planet_environment_enum ] [ std::cout << _1 << "\n" ]
+                |  as_string[ tok.universe_object_type_enum ] [ std::cout << _1 << "\n" ]
+                |  as_string[ tok.star_type_enum ] [ std::cout << _1 << "\n" ]
+                |  as_string[ tok.empire_affiliation_type_enum ] [ std::cout << _1 << "\n" ]
+                |  as_string[ tok.unlockable_item_type_enum ] [ std::cout << _1 << "\n" ]
+                |  as_string[ tok.tech_type_enum ] [ std::cout << _1 << "\n" ]
+                |  as_string[ tok.combat_fighter_type_enum ] [ std::cout << _1 << "\n" ]
+                |  as_string[ tok.ship_part_class_enum ] [ std::cout << _1 << "\n" ]
+                |  as_string[ tok.ship_slot_type_enum ] [ std::cout << _1 << "\n" ]
+                |  as_string[ tok.capture_result_enum ] [ std::cout << _1 << "\n" ]
+                |  as_string[ tok.statistic_type_enum ] [ std::cout << _1 << "\n" ]
 
 #define NAME_TOKEN(r, _, name) | tok.BOOST_PP_CAT(name, _) [ std::cout << _1 << "\n" ]
                    BOOST_PP_SEQ_FOR_EACH(NAME_TOKEN, _, NAMES_SEQ)
 #undef NAME_TOKEN
 
-                |  '='
-                |  '+'
-                |  '-'
-                |  '*'
-                |  '/'
-                |  '.'
-                |  ','
-                |  '('
-                |  ')'
-                |  '['
-                |  ']'
+                |  char_('=') [ std::cout << _1 << "\n" ]
+                |  char_('+') [ std::cout << _1 << "\n" ]
+                |  char_('-') [ std::cout << _1 << "\n" ]
+                |  char_('*') [ std::cout << _1 << "\n" ]
+                |  char_('/') [ std::cout << _1 << "\n" ]
+                |  char_('.') [ std::cout << _1 << "\n" ]
+                |  char_(',') [ std::cout << _1 << "\n" ]
+                |  char_('(') [ std::cout << _1 << "\n" ]
+                |  char_(')') [ std::cout << _1 << "\n" ]
+                |  char_('[') [ std::cout << _1 << "\n" ]
+                |  char_(']') [ std::cout << _1 << "\n" ]
               )
         ;
 
